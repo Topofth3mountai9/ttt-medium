41a9c5fb88cbb04263dad36bea5bd62495660e86
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-04-24 10:53:52 -0400

    Add gitignore and finalize

--- ttt.py
+++ ttt.py
@@ -1,22 +1,9 @@
 # ttt.py
 
-import timeit
-
 board = [["_" for _ in range(3)] for _ in range(3)]
 
-def print_board_list(board):
-    [print(row) for row in board]
-
-def print_board_for(board):
+def print_board(board):
     for row in board:
         print(row)
 
-begin = timeit.default_timer()
-print_board_list(board)
-end = timeit.default_timer()
-print(end - begin)
-
-begin = timeit.default_timer()
-print_board_for(board)
-end = timeit.default_timer()
-print(end - begin)
+print_board(board)
