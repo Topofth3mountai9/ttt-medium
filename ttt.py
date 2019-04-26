f06971f8d85258be6db673ebc0eafb25f09d628f
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-04-26 11:18:24 -0400

    Alternate players

--- ttt.py
+++ ttt.py
@@ -2,13 +2,16 @@
 
 def main():
     board = [["_" for _ in range(3)] for _ in range(3)]
+    is_x = True
     while True:
+        player = "X" if is_x else "O"
         print_board(board)
         try:
             selection = convert_selection(select_square())
-            place_piece(selection, board)
+            place_piece(selection, player, board)
         except ValueError:
             print("Sorry, please select a number 1-9")
+        is_x = not is_x
 
 
 def convert_selection(selection):
@@ -16,8 +19,8 @@ def convert_selection(selection):
     return (selection // 3, selection % 3)
 
 
-def place_piece(selection, board):
-    board[selection[0]][selection[1]] = "X"
+def place_piece(selection, player, board):
+    board[selection[0]][selection[1]] = player
 
 
 def print_board(board):
