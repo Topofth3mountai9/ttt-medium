07d97583cea87ced1684252caec4f3d4320d3ee1
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-04-29 11:00:37 -0400

    Add game_over and is_draw

--- ttt.py
+++ ttt.py
@@ -3,19 +3,30 @@
 def main():
     board = [["_" for _ in range(3)] for _ in range(3)]
     is_x = True
-    while True:
+    game_over = False
+    while not game_over:
         print_board(board)
         try:
             selection = convert_selection(select_square())
             place_piece(selection, is_x, board)
         except ValueError:
             print("Sorry, please select a square 1-9 that is unoccupied.")
+            continue
+        game_over = is_draw(board)
         is_x = not is_x
 
 
 def convert_selection(selection):
     selection -= 1
     return (selection // 3, selection % 3)
+
+
+def is_draw(board):
+    for row in board:
+        for val in row:
+            if val == "_":
+                return False
+    return True
 
 
 def place_piece(selection, is_x, board):
